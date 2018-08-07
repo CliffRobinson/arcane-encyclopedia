3c0e6ee0f9307e7608da3caba41e133c8d3ebcf0
CliffRobinson <Clifford.f.robinson@gmail.com>
2018-08-07 16:09:00 +1200

    removed pre-refactor reducers

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,19 +1,16 @@
 import {combineReducers} from 'redux'
 
 import cards from './cards'
-//import mana from './mana'
-import pointless from './pointless'
+
 import w from './mana/w'
 import u from './mana/u'
 import b from './mana/b'
 import r from './mana/r'
 import g from './mana/g'
-import total from './total'
+
 
 export default combineReducers({
     cards,
-    //mana,
-    //pointless,
     w, u, b, r, g,
 
 })
