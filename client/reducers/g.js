3cbd9a070a923ba902eca2aa487e4493f6f50f73
CliffRobinson <Clifford.f.robinson@gmail.com>
2018-08-07 15:56:23 +1200

    clear implemented in reducers

--- client/reducers/g.js
+++ client/reducers/g.js
@@ -1,13 +1,15 @@
-import { ADD_W, ADD_U, ADD_B, ADD_R, ADD_G, SUB_W, SUB_U, SUB_B, SUB_R, SUB_G, CLEAR} from '../actions/mana'
+import { ADD_W, ADD_U, ADD_B, ADD_R, ADD_G, SUB_W, SUB_U, SUB_B, SUB_R, SUB_G, CLEAR } from '../actions/mana'
 
-function g(state=0, action){
-    switch (action.type){
+function g(state = 0, action) {
+    switch (action.type) {
         case ADD_G:
-            return state+1;
+            return state + 1;
         case SUB_G:
             if (state != 0) {
-                return state-1;
+                return state - 1;
             }
+        case CLEAR:
+            return 0;
         default:
             return state;
     }
--- client/reducers/u.js
+++ client/reducers/u.js
@@ -1,13 +1,15 @@
-import { ADD_W, ADD_U, ADD_B, ADD_R, ADD_G, SUB_W, SUB_U, SUB_B, SUB_R, SUB_G, CLEAR} from '../actions/mana'
+import { ADD_W, ADD_U, ADD_B, ADD_R, ADD_G, SUB_W, SUB_U, SUB_B, SUB_R, SUB_G, CLEAR } from '../actions/mana'
 
-function u(state=0, action){
-    switch (action.type){
+function u(state = 0, action) {
+    switch (action.type) {
         case ADD_U:
-            return state+1;
+            return state + 1;
         case SUB_U:
             if (state != 0) {
-                return state-1;
+                return state - 1;
             }
+        case CLEAR:
+            return 0;
         default:
             return state;
     }
