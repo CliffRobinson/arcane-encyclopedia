54cf76b9599fc496239d59665bce89a732c46f17
CliffRobinson <Clifford.F.Robinson@gmail.com>
2018-08-12 15:44:53 +1200

    further reducer tests, odd behaviour on onlyTricks

--- tests/reducers.test.js
+++ tests/reducers.test.js
@@ -23,9 +23,99 @@ test('Test add cards case', () => {
         cardArray: fakeCards
     }
     //Act
-    console.log(fakeCards);
-    
+    //console.log(fakeCards);
     const actual = cards([], action)
+
     //Assert
     expect(actual).toEqual(expected)
+})
+
+test('Test clear cards case', ()=>{
+    //Arrange
+    const expected = [];
+    const action = {
+        type: CLEAR_CARDS
+    };
+    //Act
+    const actual = cards(fakeCards, action);
+    //Assert
+    expect(actual).toEqual(expected);
+})
+
+test('Default should return state as input', ()=>{
+    //Arrange
+    const expected = fakeCards;
+    const action = {
+        type: ""
+    };
+    //Act
+    const actual = cards(fakeCards, action);
+    //Assert
+    expect(actual).toEqual(expected);
+})
+
+//Tricks Tests:
+
+import  onlyTricks from '../client/reducers'
+import {
+    TRICKS_ON, tricksOn,
+    TRICKS_OFF, tricksOff,
+    TRICKS_TOGGLE, tricksToggle,
+} from '../client/actions/onlyTricks'
+
+test('Initial state is false', ()=>{
+    //Arrange
+    const expected = false
+    //Act
+    const actual = onlyTricks(undefined, {})
+    //Assert
+    expect(actual.onlyTricks).toEqual(expected)
+})
+
+test('tricksOn returns true', ()=>{
+    //Arrange
+    const expected = true
+    const action =  {
+        type:TRICKS_ON
+    }
+    //Act
+    const actual = onlyTricks(undefined, action)
+    //Assert
+    expect(actual.onlyTricks).toEqual(expected)
+})
+
+test('tricksOff returns false', ()=>{
+    //Arrange
+    const expected = false
+    const action =  {
+        type:TRICKS_OFF
+    }
+    //Act
+    const actual = onlyTricks(undefined, action)
+    //Assert
+    expect(actual.onlyTricks).toEqual(expected)
+})
+
+test('tricksToggle switches state from false to true', ()=>{
+    //Arrange
+    const expected = true
+    const action =  {
+        type:TRICKS_TOGGLE
+    }
+    //Act
+    const actual = onlyTricks(undefined, action)
+    //Assert
+    expect(actual.onlyTricks).toEqual(expected)
+})
+
+test('tricksToggle switches state from true to false', ()=>{
+    //Arrange
+    const expected = false
+    const action =  {
+        type:TRICKS_TOGGLE
+    }
+    //Act
+    const actual = onlyTricks({onlyTricks:true}, action)
+    //Assert
+    expect(actual.onlyTricks).toEqual(expected)
 })
