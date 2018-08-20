f49d84ea422e7c69ec770960bc460bac22a38f25
CliffRobinson <Clifford.f.robinson@gmail.com>
2018-08-20 19:53:27 +1200

    tests and functions for sort, alpha sort behaving strangely

--- client/reducers/sort.js
+++ client/reducers/sort.js
@@ -2,25 +2,79 @@ import { SORT_CMC, SORT_COLOR, SORT_NAME, SORT_PRICE, sortCMC, sortColor, sortNa
 
 //These functions will be held in redux state, hence the name. 
 export function reduxCompareCMC(cardA, cardB) {
-
+    let diff = cardA.cmc - cardB.cmc;
+    if (diff == 0){
+        return reduxCompareName(cardA, cardB);
+    } else return diff;
 }
 
 export function reduxCompareColor(cardA, cardB) {
+    let diff = getColorSortIndex(cardA) - getColorSortIndex(cardB);
+    if (diff == 0){
+        return reduxCompareName(cardA, cardB);
+    } else return diff;
+}
 
+function getColorSortIndex(card) {
+    let cardColors;
+
+    switch (card.layout){
+    case "transform":
+        cardColors = card.card_faces[0].colors;
+        break;
+    default:
+        cardColors = card.colors;
+    }
+
+    if (cardColors.length == 0){
+        return 7;
+    } else if (cardColors.length > 1) {
+        return 6;
+    } else switch(cardColors[0]){
+    case "W":
+        return 1;
+    case "U":
+        return 2;
+    case "B":
+        return 3;
+    case "R":
+        return 4;
+    case "G":
+        return 5;
+    default:
+        console.log(`${card.name} has diamond mana, or something is broken.`);
+        return 7;
+    }
 }
 
 export function reduxComparePrice(cardA, cardB) {
-
+    let diff = cardA.usd - cardB.usd;
+    /*if (diff == 0){
+        return reduxCompareName(cardA, cardB);
+    } else */ return diff;
 }
 
 export function reduxCompareName(cardA, cardiB) {
-
+    if (cardA.name < cardiB){
+        return -1;
+    } else if (cardA.name > cardiB) {
+        return 1;
+    } else {
+        console.log("Sort function found 2 cards with the same name. This should not be possible, all MTG cards have unique names.");
+        return 0;
+    }
 } //Cards will be sorted by name by default, so this is mainly a tiebreaker.
 
 export function sort(state = undefined, action) {
     switch (action.type) {
     case SORT_CMC:
         return reduxCompareCMC;
+    case SORT_COLOR:
+        return reduxCompareColor;
+    case SORT_NAME:
+        return reduxCompareName;
+    case SORT_PRICE:
+        return reduxComparePrice;
     default:
         return state;
     }
--- tests/reducers.sort.test.js
+++ tests/reducers.sort.test.js
@@ -1,6 +1,8 @@
 import {sort, reduxCompareCMC, reduxCompareColor, reduxCompareName, reduxComparePrice} from "../client/reducers/sort";
 
-import {SORT_CMC, SORT_COLOR, SORT_NAME, SORT_PRICE, sortCMC, sortColor, sortName, sortPrice} from "../client/actions/sort";
+import {SORT_CMC, SORT_COLOR, SORT_NAME, SORT_PRICE, } from "../client/actions/sort";
+
+import {data as fakeCards}  from "../tests/testData.json";
 
 // test("Test sort _thing case", () => {
 //     //Arrange
@@ -60,4 +62,79 @@ test("Test sort Price case", () => {
     const actual = sort(undefined, action);
     //Assert
     expect(actual).toEqual(expected);
+});
+
+test("reduxCompareName sorts correctly", ()=> {
+    //Arrange
+    const expected = fakeCards.slice();
+    //Act
+    let actual = fakeCards.slice().reverse().sort(reduxCompareName);
+    //clone, reverse, and then sort the array.
+    //Assert
+    const expectedNames = expected.map(card => card.name);
+    const actualNames = actual.map(card => card.name);
+    expect(actualNames).toEqual(expectedNames);
+});
+
+test("reduxCompareCMC sorts correctly", ()=> {
+    //Arrange
+    const expected = [
+        fakeCards[0], //Arch: 0
+        fakeCards[1], //Die: 2
+        fakeCards[3], //Essence Scatter: 2
+        fakeCards[6], //L. Cubby: 2
+        fakeCards[5], //G. Chainwhirler: 3
+        fakeCards[7], //Nicky B: 4
+        fakeCards[4], //Garna: 5
+        fakeCards[8], //Prepare2Fight: 6
+        fakeCards[2], //Dusk2Dawn: 9
+    ];
+    //Act
+    let actual = fakeCards.slice().reverse().sort(reduxCompareCMC);
+    //Assert
+    const expectedNames = expected.map(card => card.name);
+    const actualNames = actual.map(card => card.name);
+    expect(actualNames).toEqual(expectedNames);
+});
+
+test("reduxComparePrice sorts correctly", ()=> {
+    //Arrange
+    const expected = [
+        fakeCards[1], //Die Young, 0.03
+        fakeCards[4], //Garna, 0.08
+        fakeCards[6], //L. Cubby, 0.08
+        fakeCards[3], //E. scat, 0.09
+        fakeCards[8], //Prepare2fight, 0.16
+        fakeCards[2], //Dusk2Dawn, 0.99
+        fakeCards[0], //Arch, 1.82
+        fakeCards[5], //g chainz, 3.82
+        fakeCards[7], //nicky B, 35.97
+    ];
+    //Act
+    let actual = fakeCards.slice().sort(reduxComparePrice);
+    //Assert
+    const expectedNames = expected.map(card => card.name);
+    const actualNames = actual.map(card => card.name);
+    expect(actualNames).toEqual(expectedNames);
+});
+
+test("reduxCompareColor sorts correctly", ()=>{
+    //Arrange
+    const expected = [
+        fakeCards[2], //D2D, W
+        fakeCards[3], //Escat, U
+        fakeCards[1], //Die young, B
+        fakeCards[5], //g Chainz, R
+        fakeCards[6], //l cubby, G
+        fakeCards[4], //Garna, gold
+        fakeCards[7], //nicky, gold
+        fakeCards[8], //prep2fight, gold
+        fakeCards[0], //arch, colorless
+    ];
+    //Act
+    let actual = fakeCards.slice().sort(reduxCompareColor);
+    //Assert
+    const expectedNames = expected.map(card => card.name);
+    const actualNames = actual.map(card => card.name);
+    expect(actualNames).toEqual(expectedNames);
 });
