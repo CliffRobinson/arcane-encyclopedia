cf4dfa330aa1c2e2797958027df7bca2fda451d4
CliffRobinson <Clifford.f.robinson@gmail.com>
2018-08-23 16:23:24 +1200

    filterbar holds info from child filters

--- client/components/FilterBar.jsx
+++ client/components/FilterBar.jsx
@@ -12,25 +12,46 @@ class FilterBar extends React.Component {
             filters:[]
         };
         this.addFilter = this.addFilter.bind(this);
+        this.modifyNum = this.modifyNum.bind(this);
     }
 
     addFilter(filter, i) {
         let newFilters = this.state.filters.slice();
-        newFilters.push(filter);
+        newFilters[i] = filter;
         this.setState({
             filters: newFilters
         });
     }
 
+    modifyNum(addNotSubtract){
+        let newNum;
+        if(addNotSubtract) {
+            newNum = this.state.num+1;
+        } else {
+            newNum = this.state.num-1;
+        }
+
+        let newFilters = new Array(newNum).fill(null);
+        newFilters = newFilters.map((newFilter, i) => {
+            if (this.state.filters[i]){
+                newFilter = this.state.filters[i];
+            }
+        });
+        this.setState({
+            num: newNum,
+            filters: newFilters
+        });
+    }
+
     render() {
-        const {num} = this.state; 
+        const {num, filters} = this.state; 
         const counter = new Array(num).fill("butts");
         console.log("Filters are", this.state.filters);
         return (
             <div className="filterBar">
                 <p>Here is the filter bar, num is {num} </p>
-                <button onClick={() => this.setState({num: num+1})} >Add a Filter </button>
-                <button onClick={() => this.setState({num: num-1})} disabled={num < 1} >Remove a Filter </button>
+                <button onClick={() => this.modifyNum(true)} >Add a Filter </button>
+                <button onClick={() => this.modifyNum(false)} disabled={num < 1} >Remove a Filter </button>
                 { counter.map((e, i) => <Filter addFilter={this.addFilter}key={i} i={i} />)}
             </div>
         );
