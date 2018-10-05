ba6f9522498e72b5b9cc2f0aefeca0d0a76467ce
CliffRobinson <Clifford.f.robinson@gmail.com>
2018-10-05 19:15:00 +1300

    app component tested

--- tests/app.test.js
+++ tests/app.test.js
@@ -6,3 +6,9 @@ import App from "../client/components/App";
 test("Test Suite Running", () => {
     expect(true).toBeTruthy();
 });
+
+test("App matches snapshot", ()=> {
+    expect(
+        toJson(shallow(<App />))
+    ).toMatchSnapshot();
+});
