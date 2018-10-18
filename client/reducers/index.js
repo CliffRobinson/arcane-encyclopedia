a51adb523462aa426cc2a2b9aed2813544cc1261
CliffRobinson <Clifford.f.robinson@gmail.com>
2018-10-18 15:09:49 +1300

    add total to combined reducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -9,6 +9,7 @@ import r from "./mana/r";
 import g from "./mana/g";
 import c from "./mana/c";
 import gold from "./mana/gold";
+import total from "./mana/total";
 
 
 import onlyTricks from "./onlyTricks";
@@ -21,7 +22,7 @@ export default combineReducers({
     format,
     cards,
     w, u, b, r, g, c,
-    gold,
+    gold, total,
     onlyTricks,
     filterLands,
     sort, 
