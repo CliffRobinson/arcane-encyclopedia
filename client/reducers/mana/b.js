12a837cced9454ffbde0f7292b556489781b6352
CliffRobinson <Clifford.f.robinson@gmail.com>
2018-10-18 15:51:13 +1300

    edited u and b to receive dimir action

--- client/reducers/mana/b.js
+++ client/reducers/mana/b.js
@@ -1,9 +1,11 @@
-import { ADD_B, SUB_B, CLEAR_MANA } from "../../actions/mana";
+import { ADD_B, SUB_B, ADD_DIMIR, SUB_DIMIR, CLEAR_MANA } from "../../actions/mana";
 
 function b(state = 0, action) {
     switch (action.type) {
+    case ADD_DIMIR:
     case ADD_B:
         return state + 1;
+    case SUB_DIMIR:
     case SUB_B:
         if (state != 0) {
             return state - 1;
--- client/reducers/mana/u.js
+++ client/reducers/mana/u.js
@@ -1,9 +1,11 @@
-import { ADD_U, SUB_U, CLEAR_MANA } from "../../actions/mana";
+import { ADD_U, SUB_U, ADD_DIMIR, SUB_DIMIR, CLEAR_MANA } from "../../actions/mana";
 
 function u(state = 0, action) {
     switch (action.type) {
+    case ADD_DIMIR:
     case ADD_U:
         return state + 1;
+    case SUB_DIMIR:
     case SUB_U:
         if (state != 0) {
             return state - 1;
