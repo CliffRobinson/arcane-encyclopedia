d143288a48d15214f3352ec95e7ca8735f6c5939
CliffRobinson <Clifford.F.Robinson@gmail.com>
2018-10-22 19:22:04 +1300

    add hybrid mana buttons

--- client/components/ManaSelector.jsx
+++ client/components/ManaSelector.jsx
@@ -21,64 +21,41 @@ import {ManaRow} from "./manaRow";
 export function ManaSelector(props) {
     return (
         <div className="manaSelector">
-            {/* <div className="manaRow">
-                <img className="manaButton" src="/images/mtga-button-transparent.png" onClick={ ()=> props.dispatch(addW())} />
-                <span  className="manaContainer">
-                    <i className="ms ms-w ms-cost ms-shadow"></i>
-                    <span className="manaText">{props.mana.w}</span> 
-                </span>
-                <img className="manaButton" src="/images/mtga-button-minus.png" onClick={ ()=> props.dispatch(subW())} />
-            </div> */}
+
             <ManaRow icon="ms ms-w ms-cost ms-shadow" add = {() => props.dispatch(addW())} sub ={() => props.dispatch(subW())}/>
-            {/* <div className="manaRow">
-                <img className="manaButton" src="/images/mtga-button-transparent.png" onClick={ ()=> props.dispatch(addU())} />
-                <span  className="manaContainer">
-                    <i className="ms ms-u ms-cost ms-shadow"></i>
-                    <span className="manaText">{props.mana.u}</span> 
-                </span>
-                <img className="manaButton" src="/images/mtga-button-minus.png" onClick={ ()=> props.dispatch(subU())} />
-            </div> */}
+
             <ManaRow icon="ms ms-u ms-cost ms-shadow" add = {() => props.dispatch(addU())} sub ={() => props.dispatch(subU())}/>
-            {/* <div className="manaRow">
-                <img className="manaButton" src="/images/mtga-button-transparent.png" onClick={ ()=> props.dispatch(addB())} />
-                <span  className="manaContainer">
-                    <i className="ms ms-b ms-cost ms-shadow"></i>
-                    <span className="manaText">{props.mana.b}</span> 
-                </span>
-                <img className="manaButton" src="/images/mtga-button-minus.png" onClick={ ()=> props.dispatch(subB())} />
-            </div> */}
+
             <ManaRow icon="ms ms-b ms-cost ms-shadow" add = {() => props.dispatch(addB())} sub ={() => props.dispatch(subB())}/>
-            {/* <div className="manaRow">
-                <img className="manaButton" src="/images/mtga-button-transparent.png" onClick={ ()=> props.dispatch(addR())} />
-                <span  className="manaContainer">
-                    <i className="ms ms-r ms-cost ms-shadow"></i>
-                    <span className="manaText">{props.mana.r}</span> 
-                </span>
-                <img className="manaButton" src="/images/mtga-button-minus.png" onClick={ ()=> props.dispatch(subR())} />
-            </div> */}
+
             <ManaRow icon="ms ms-r ms-cost ms-shadow" add = {() => props.dispatch(addR())} sub ={() => props.dispatch(subR())}/>
-            {/* <div className="manaRow">
-                <img className="manaButton" src="/images/mtga-button-transparent.png" onClick={ ()=> props.dispatch(addG())} />
-                <span  className="manaContainer">
-                    <i className="ms ms-g ms-cost ms-shadow"></i>
-                    <span className="manaText">{props.mana.g}</span> 
-                </span>
-                <img className="manaButton" src="/images/mtga-button-minus.png" onClick={ ()=> props.dispatch(subG())} />
-            </div> */}
+
             <ManaRow icon="ms ms-g ms-cost ms-shadow" add = {() => props.dispatch(addG())} sub ={() => props.dispatch(subG())}/>
-            {/* <div className="manaRow">
-                <img className="manaButton" src="/images/mtga-button-transparent.png" onClick={ ()=> props.dispatch(addC())} />
-                <span  className="manaContainer">
-                    <i className="ms ms-c ms-cost ms-shadow"></i>
-                    <span className="manaText">{props.mana.c}</span> 
-                </span>
-                <img className="manaButton" src="/images/mtga-button-minus.png" onClick={ ()=> props.dispatch(subC())} />
-            </div> */}
+
             <ManaRow icon="ms ms-c ms-cost ms-shadow" add = {() => props.dispatch(addC())} sub ={() => props.dispatch(subC())}/>
             <div>
                 <button className="button is-small" onClick={ ()=> props.dispatch(clearMana())}> Clear Mana</button>
             </div>
+
+            <ManaRow icon="ms ms-wu ms-cost ms-shadow" add = {() => props.dispatch(addAzorius())} sub ={() => props.dispatch(subAzorius())}/>
+
+            <ManaRow icon="ms ms-wb ms-cost ms-shadow" add = {() => props.dispatch(addOrzhov())} sub ={() => props.dispatch(subOrzhov())}/>
+
+            <ManaRow icon="ms ms-ub ms-cost ms-shadow" add = {() => props.dispatch(addDimir())} sub ={() => props.dispatch(subDimir())}/>
+
+            <ManaRow icon="ms ms-ur ms-cost ms-shadow" add = {() => props.dispatch(addIzzet())} sub ={() => props.dispatch(subIzzet())}/>
+
+            <ManaRow icon="ms ms-br ms-cost ms-shadow" add = {() => props.dispatch(addRakdos())} sub ={() => props.dispatch(subRakdos())}/>
+
+            <ManaRow icon="ms ms-bg ms-cost ms-shadow" add = {() => props.dispatch(addGolgari())} sub ={() => props.dispatch(subGolgari())}/>
+
+            <ManaRow icon="ms ms-rg ms-cost ms-shadow" add = {() => props.dispatch(addGruul())} sub ={() => props.dispatch(subGruul())}/>
+
             <ManaRow icon="ms ms-rw ms-cost ms-shadow" add = {() => props.dispatch(addBoros())} sub ={() => props.dispatch(subBoros())}/>
+
+            <ManaRow icon="ms ms-gw ms-cost ms-shadow" add = {() => props.dispatch(addSelesnya())} sub ={() => props.dispatch(subSelesnya())}/>
+
+            <ManaRow icon="ms ms-gu ms-cost ms-shadow" add = {() => props.dispatch(addSimic())} sub ={() => props.dispatch(subSimic())}/>
             
         </div>
     );
